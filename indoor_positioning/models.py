54a17d5989c083dbfb4e25f8d84ab6975586cdc5
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2022-04-07 16:02:34 +0800

    增加数据模型

--- indoor_positioning/models.py
+++ indoor_positioning/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import json
 
 # Create your models here.
+class Data(models.Model):
+    class Meta:
+        verbose_name = '数据'
+        verbose_name_plural = verbose_name
+        ordering = ['-time']
+
+    id = models.BigAutoField(primary_key=True)
+    raw_data = models.TextField('原数据')
+    time = models.DateTimeField('创建时间', auto_now_add=True)
+
+    @property
+    def data(self):
+        return json.loads(self.raw_data)
+
+    @data.setter
+    def data(self, val):
+        self.raw_data = json.dumps(val)
+
+
+class WifiData(Data):
+    class Meta:
+        verbose_name = 'WiFi数据'
+        verbose_name_plural = verbose_name
