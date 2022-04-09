3b5c40c90680a56e575ec3faa950c3ed6d921aec
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2022-04-09 20:43:22 +0800

    Update admin.py

--- indoor_positioning/admin.py
+++ indoor_positioning/admin.py
@@ -4,7 +4,7 @@ from indoor_positioning.models import Data, WifiData
 # Register your models here.
 @admin.register(Data)
 class DataAdmin(admin.ModelAdmin):
-    list_display = ['id', 'time', 'raw_data']
+    list_display = ['id', 'time', 'json_data']
     list_filter = ['time']
     date_hierarchy = 'time'
 
